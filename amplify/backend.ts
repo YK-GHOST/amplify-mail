35e41aaade2d7dcd6d74e3603abc593936c0fed3
YK-GHOST <yogeshkantiwal20@gmail.com>
2024-12-08 22:11:08 +0530

    commit 4

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { myFirstFunction } from "./my-first-function/resource";
+import { sayHello } from "./functions/sayHello/resource";
 
 defineBackend({
   auth,
   data,
-  myFirstFunction,
+  sayHello,
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import outputs from "../amplify_outputs.json";
+// import { Authenticator } from "@aws-amplify/ui-react";
 import App from "./App.tsx";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import type { Schema } from ".././amplify/data/resource";
 
 Amplify.configure(outputs);
 
+const client = generateClient<Schema>();
+console.log(client.queries);
+
+client.queries.sayHello({
+  name: "Amplify",
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>
 );
